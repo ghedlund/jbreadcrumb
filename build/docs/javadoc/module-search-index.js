236651aaae16017763b1b882a5086b0ed5df5bc0
Gregory Hedlund <greg.hedlund@gmail.com>
2026-01-27 15:15:04 -0330

    switch to gradle

--- build/docs/javadoc/module-search-index.js
+++
@@ -1,1 +0,0 @@
-moduleSearchIndex = [{"l":"ca.phon.jbreadcrumb"}];updateSearchResults();
--- build/docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"m":"ca.phon.jbreadcrumb","l":"ca.phon.ui.jbreadcrumb"}];updateSearchResults();
